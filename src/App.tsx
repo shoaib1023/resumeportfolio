b6e477fcc261a41239aedeba5164ee6e9fc0e7a0
Shoaib Ur Rehman <shoaib.rehman98@gmail.com>
2026-02-19 14:55:28 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ContactSection from "./sections/contact-section";
 import EducationSection from "./sections/education-section";
 import ExperienceSection from "./sections/experience-section";
 import HeroSection from "./sections/hero-section";
-import ProjectsSection from "./sections/projects-section";
+// import ProjectsSection from "./sections/projects-section";
 import SkillsSection from "./sections/skills-section";
 
 export default function App() {
